4023a3c979217f498dd9a40fc821b25b8a7c32a8
Jizan Diaz Villafuerte <diazjizan@gmail.com>
2024-03-30 02:18:54 +0800

    Improve loading screen

--- src/pages/mobileapp.js
+++ src/pages/mobileapp.js
@@ -18,15 +18,17 @@ const Mobileapp = () => {
   return (
     <div>
       <div
-        className="absolute opacity-100 top-0 left-0 flex justify-center items-center w-screen h-screen bg-blue-50 z-[1000] transition-opacity duration-500 dark:bg-darkBG dark:text-darkLight"
+        className="bg-blue-50 fixed w-screen h-screen dark:bg-darkBG z-[1000]"
         id="loader"
       >
-        <InfinitySpin
-          visible={true}
-          width="200"
-          color="currentColor"
-          ariaLabel="infinity-spin-loading"
-        />
+        <div className="absolute opacity-100 top-0 left-0 flex justify-center items-center w-screen h-screen bg-blue-50 z-[1000] transition-opacity duration-500 dark:bg-darkBG dark:text-darkLight">
+          <InfinitySpin
+            visible={true}
+            width="200"
+            color="currentColor"
+            ariaLabel="infinity-spin-loading"
+          />
+        </div>
       </div>
       <Header />
       <Hero />
